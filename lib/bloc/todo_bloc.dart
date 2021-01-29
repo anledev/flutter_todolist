8461c45bf2084abb5f5e28680dca65b82cdf363e
anlt <thanhan.le21@gmail.com>
2021-01-29 17:25:21 +0700

    add repo

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -1,54 +1,51 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:flutter_todo_list/db/todo_table.dart';
-import 'package:flutter_todo_list/event/add_todo_event.dart';
-import 'package:flutter_todo_list/event/delete_todo_event.dart';
+import '../event/add_todo_event.dart';
+import '../event/delete_todo_event.dart';
+import '../repo/todo_repo.dart';
 
 import '../base/base_bloc.dart';
 import '../base/base_event.dart';
 import '../model/todo.dart';
 
 class TodoBloc extends BaseBloc {
-
-  TodoTable _todoTable = TodoTable();
+  TodoRepo _todoRepo = TodoRepo();
 
   StreamController<List<Todo>> _todoListStreamController =
   StreamController<List<Todo>>();
-
   Stream<List<Todo>> get todoListStream => _todoListStreamController.stream;
 
   var _randomInt = Random();
   List<Todo> _todoListData = List<Todo>();
 
   initData() async {
-    _todoListData = await _todoTable.selectAllTodo();
-    if (_todoListData == null) {
-      return;
-    }
-    _todoListStreamController.sink.add(_todoListData);
+    var data = await _todoRepo.initData();
+    _todoListStreamController.sink.add(data);
   }
 
   _addTodo(Todo todo) async {
-    //insert to database
-    await _todoTable.insertTodo(todo);
+    // insert to database
+    await _todoRepo.insertTodo(todo);
 
     _todoListData.add(todo);
     _todoListStreamController.sink.add(_todoListData);
   }
 
   _deleteTodo(Todo todo) async {
-    await _todoTable.deleteTodo(todo);
+    await _todoRepo.deleteTodo(todo);
 
     _todoListData.remove(todo);
     _todoListStreamController.sink.add(_todoListData);
   }
 
-
   @override
   void dispatchEvent(BaseEvent event) {
     if (event is AddTodoEvent) {
-      Todo todo = new Todo.fromData(_randomInt.nextInt(1000), event.content);
+      Todo todo = Todo.fromData(
+        _randomInt.nextInt(1000000),
+        event.content,
+      );
       _addTodo(todo);
     } else if (event is DeleteTodoEvent) {
       _deleteTodo(event.todo);
@@ -57,7 +54,6 @@ class TodoBloc extends BaseBloc {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     _todoListStreamController.close();
   }
